e52b88c0e83b7c97ada6a2d6240ec5006722e600
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2013-02-15 12:21:18 -0500

    migration add price to line_item, add_product and total_price were  modified in cart model and too the show view in cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,13 +1,21 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
   
-  def add_product(product_id)
-    @current_item = line_items.where(product_id: product_id).first
+  def add_product(product)
+    @current_item = line_items.where(product_id: product.id).first
     if @current_item
       @current_item.quantity += 1
+      @current_item.price += product.price
     else
-      @current_item = line_items.build(product_id: product_id)
+      @current_item = line_items.build(product_id: product.id, price: product.price)
     end
     @current_item
+  end
+  
+  def total_price
+    line_items.to_a.sum do |item|
+      #item.total_price
+      item.price
+    end
   end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,9 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id, :quantity, :product, :cart
+  attr_accessible :cart_id, :product_id, :quantity, :price, :product, :cart
   belongs_to :product
   belongs_to :cart
+  
+  #def total_price
+  #  price
+  #end
 end
