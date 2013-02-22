e6ab9a22c5656e9cc2909f73b62ad1e13a1db775
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2013-02-22 18:41:18 -0500

    new mailer added, development and production were configured to send email with gmail smtp when an order is place

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,19 @@ Depot::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
   # Enable threaded mode
   # config.threadsafe!
