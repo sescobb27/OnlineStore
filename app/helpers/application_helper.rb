6985e43f3551c1e2c729f8a75f8a258a00746da9
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2013-03-02 13:36:03 -0500

    internationalization allready finish, one more application helper for hidding login link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,11 @@ module ApplicationHelper
     end
     content_tag("div", attributes, &block)
   end
+
+  def hidden_link_if(condition, attributes = {})
+  	if condition
+  		attributes["style"] = "display: none"
+  	end
+  	content_tag("div", link_to(t('.log_in'), '#',attributes))
+  end
 end
